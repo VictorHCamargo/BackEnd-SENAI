3845a19cab37978223bd57482552833f557ed0b1
VictorHCamargo <victor.h0786@gmail.com>
2025-08-11 16:29:21 -0300

    Adicionado a função readline, para otimização entre máquina e usuário.

--- AULAS/AULA_3/condicionais.php
+++ AULAS/AULA_3/condicionais.php
@@ -1,20 +1,19 @@
 <?php
-    $nome = "";
-    $nota1 = 9;
-    $nota2 = 0;
+    $nome = readline("Digite seu nome: \n");
+    $nota1 = readline("Digite a 1º nota do aluno: \n");
+    $nota2 = readline("Digite a 2ª nota do aluno: \n");
     $media = ($nota1 + $nota2)/2;
     //Se a presença do aluno for menor que 75% será reprovado automaticamente
-    $presenca = 80/100;
+    $presenca = readline("Digite a presença do aluno: \n");
     if ($nome == "") {
         /*Para realizar a condição deve ser chamada a variável 
         e logo depois comparada com algum valor */
-        if ($media >= 7 && $presenca >= 0.75) {
-            echo "Aprovado por média e presença exelente";
+        if ($media >= 7 && ($presenca/100) >= 0.75) {
+            echo "Aprovado com uma média de $media e com uma presença $presenca%";
         } else if ($media < 7 && $media >=5 && $presenca >= 0.75) {
-            echo "Recuperação por média baixa";
-        } else echo "Reprovado por presença ou média vermelha";
+            echo "Recuperação com uma média de $media";
+        } else echo "Reprovado por presença baixa ($presenca%) ou por média baixa ($media)";
     } else {
         echo "Aprovado por nome bonito";
     }
-    
 ?>
